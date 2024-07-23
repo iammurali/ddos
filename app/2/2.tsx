9c5315d2c95131ab667df18e42b3dba6b74ec7c5
iammurali <muralismail4u@gmail.com>
2024-07-23 23:43:21 +0530

    add two more animation

---
+++ app/2/2.tsx
@@ -0,0 +1,32 @@
+import BlurFade from "@/components/magicui/blur-fade";
+import BlurIn from "@/components/magicui/blur-in";
+import Particles from "@/components/magicui/particles";
+import TypingAnimation from "@/components/magicui/typing-animation";
+import { ParticlesComp } from "@/components/particle";
+
+export default function TWO() {
+  return (
+    <main className="relative w-full flex min-h-screen flex-col items-center justify-center">
+      {/* <ParticlesComp /> */}
+      {/* <TypingAnimation
+          duration={100}
+          className="text-lg font-bold text-black dark:text-white"
+          text="Remember, every failure is just a step towards your next epic disaster."
+        /> */}
+      <BlurIn
+      word="When life feels hard, Give up"
+      className="text-lg font-bold text-black dark:text-white"
+    />
+      {/* <BlurFade delay={0.25} inView>
+        <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
+          When life feels hard
+        </h2>
+      </BlurFade>
+      <BlurFade delay={0.25 * 2} inView>
+        <span className="text-xl text-pretty tracking-tighter sm:text-xl xl:text-2xl/none">
+          Give up
+        </span>
+      </BlurFade> */}
+    </main>
+  );
+}
